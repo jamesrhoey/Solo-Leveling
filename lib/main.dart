b87547edb6e3cafbb805fa98a4dcaef1ed0a9aa3
jamesrhoey <jamesrhoeydecastro7@gmail.com>
2025-06-19 10:16:25 +0800

    day2

--- lib/main.dart
+++ lib/main.dart
@@ -9,35 +9,53 @@ void main() {
           backgroundColor: const Color.fromARGB(255, 45, 162, 22),
           shadowColor: const Color.fromARGB(255, 121, 3, 25),
           surfaceTintColor: Colors.black87,
-          actions: [BackButton()],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'HAHAHAHHA',
-                style: TextStyle(
-                  fontSize: 10,
-                  backgroundColor: const Color.fromARGB(255, 193, 0, 0),
-                ),
+        body: Column(
+          children: [
+            Container(
+              
+              margin: EdgeInsets.all(10),
+              child: Row(
+                
+                children: [
+                  Text(
+                    style: TextStyle(fontSize: 20),
+                    
+                    'Name:'),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    child: Text(
+                      style: TextStyle(color: const Color.fromARGB(255, 255, 0, 0)),
+                      'James Rhoey P. De Castro'),
+                  ),
+                ],
               ),
-              Text(
-                'HAHAHAHHA',
-                style: TextStyle(
-                  fontSize: 20,
-                  backgroundColor: const Color.fromARGB(255, 251, 255, 0),
-                ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+              
+              child: Row(
+                children: [
+                  Text(style: TextStyle(fontSize: 20),'Age:'),
+                  Text(style: TextStyle(color: const Color.fromARGB(255, 255, 0, 0)),'21 years old'),
+                ],
               ),
-              Text(
-                'HAHAHAHHA',
-                style: TextStyle(
-                  fontSize: 30,
-                  backgroundColor: const Color.fromARGB(255, 19, 223, 1),
-                ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
+              
+              child: Row(
+                children: [
+                  Text(style: TextStyle(fontSize: 20),'Gender:'),
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    child: Text(style: TextStyle(color: const Color.fromARGB(255, 255, 0, 0)),'Male'),
+                  ),
+                  
+                ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     ),
