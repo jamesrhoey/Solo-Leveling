c512296b82edb32c41efbba41311e825e7559199
jamesrhoey <jamesrhoeydecastro7@gmail.com>
2025-06-25 09:52:28 +0800

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,178 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Solo Leveling'),
-          backgroundColor: const Color.fromARGB(255, 45, 162, 22),
-          shadowColor: const Color.fromARGB(255, 121, 3, 25),
-          surfaceTintColor: Colors.black87,
-          actions: [BackButton()],
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'HAHAHAHHA',
-                style: TextStyle(
-                  fontSize: 10,
-                  backgroundColor: const Color.fromARGB(255, 193, 0, 0),
+  runApp(MaterialApp(home: Profile()));
+}
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Questify'),
+        backgroundColor: const Color.fromARGB(255, 45, 162, 22),
+        shadowColor: const Color.fromARGB(255, 121, 3, 25),
+        surfaceTintColor: Colors.black87,
+      ),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(0, 50, 0, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'User Profile',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            Icon(Icons.account_circle, size: 100),
+            Container(
+              padding: EdgeInsets.fromLTRB(30, 20, 0, 0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'Name',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(30, 5, 0, 0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'James Rhoey P. De Castro',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(30, 10, 0, 0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'Email',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(30, 5, 0, 0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'jamesrhoeydecastro7@gmail.com',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(30, 10, 0, 0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'Company',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(30, 5, 0, 0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'Batangas State University TNEU Balayan',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(30, 10, 0, 0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'Contact Number',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(30, 5, 0, 0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        '09777404043',
+                        style: TextStyle(fontStyle: FontStyle.italic),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(0, 170, 0, 0),
+              padding: EdgeInsets.all(10),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+
+                  children: [Icon(Icons.logout), Text('Logout')],
                 ),
               ),
-              Text(
-                'HAHAHAHHA',
-                style: TextStyle(
-                  fontSize: 20,
-                  backgroundColor: const Color.fromARGB(255, 251, 255, 0),
-                ),
-              ),
-              Text(
-                'HAHAHAHHA',
-                style: TextStyle(
-                  fontSize: 30,
-                  backgroundColor: const Color.fromARGB(255, 19, 223, 1),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
-    ),
-  );
+    );
+  }
 }
